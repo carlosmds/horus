ea607ee456231a462eead05f981130804b396cf2
Carlos Medeiros <carlosmedeirosdesouza@gmail.com>
2020-06-24 23:50:36 -0300

    update

--- src/routes/CreateRoom.js
+++ src/routes/CreateRoom.js
@@ -18,10 +18,15 @@ export default function CreateRoom (props) {
 
     const cardStyles = {
         p: 2,
-        boxShadow: '0 0 4px rgba(145, 145, 145, .500)'
+        boxShadow: '0 0 4px rgba(145, 145, 145, .500)',
+        borderRadius: '10px',
     };
 
-    const [name, setName] = useState(props.name);    
+    const linkStyles = { display: 'inline-block', fontWeight: 'bold', px: 2, py: 1, color: 'inherit', mt:1, p:2, pt:2, pb:1 };
+
+    const [name, setName] = useState(props.name); 
+    
+    var nameInput;
 
     useEffect(() => {
         storage.setItem('userName', name);
@@ -36,8 +41,11 @@ export default function CreateRoom (props) {
         <Box p={4} textAlign='center'>
             
             <Heading> Olá,
-                <Input id="name" name="name" display='inline' sx={inputStyles} width={1/9} defaultValue={name} placeholder='você' onChange={ e => setName(e.target.value) }/>
-                <FaPencilAlt/>
+                <Input name='name' display='inline' sx={inputStyles} width={1/8} defaultValue={name} placeholder='seu nome, queridx' 
+                    onChange={ e => setName(e.target.value) }
+                    ref={(input) => { nameInput = input; }}
+                />
+                <Link sx={linkStyles} href='#'><FaPencilAlt onClick={ e => nameInput.focus()}/></Link> 
             </Heading>
 
             <Flex p={4}>
